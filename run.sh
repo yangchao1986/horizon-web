d6ded01572ec97e1145e4d6c87307c12fe067788
yangchao1986 <yangchao_x985@163.com>
2022-07-29 11:48:26 +0800

    msg

--- run.sh
+++ run.sh
@@ -6,11 +6,11 @@ npm run build
 echo '创建镜像'
 docker build -t horizonsys/web:v1. . 
 echo '删除旧容器'
-exist=`docker inspect --format '{{.State.Running}}' ${horizonsys-web}`
+exist=`docker inspect --format '{{.State.Running}}' horizon-web`
 if [ "$exist" ];
 then
-docker stop horizonsys-web
-docker rm horizonsys-web
+docker stop horizon-web
+docker rm horizon-web
 fi;
 echo '创建新容器'
-docker docker run -di --name=horizonsys-server -p 30080:80 horizonsys/web:v1.0
+docker docker run -di --name=horizonsys-web -p 30080:80 horizonsys/web:v1.0
